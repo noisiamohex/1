cd2fd13945f83a46414c0a0908c7f666dc031e63
daniele-dellorto <37075250+daniele-dellorto@users.noreply.github.com>
2020-01-26 20:30:16 +0100

    allProve 1

---
+++ sketch_allProve.js
@@ -0,0 +1,17 @@
+preload = function() {
+}
+
+setup = function() {
+}
+
+draw = function() {
+}
+
+function findIndizio(){
+  document.getElementById("indizio").style.opacity = 1;
+}
+
+function mousePressed(){
+  document.getElementById("error").style.opacity = 0;
+  document.getElementById("indizio").style.opacity = 0;
+}

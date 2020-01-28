4cd5558e5daf858f2d5d12a3f8472561f8480356
daniele-dellorto <37075250+daniele-dellorto@users.noreply.github.com>
2020-01-28 14:04:01 +0100

    iOS

--- istruzioni/app_istr.js
+++ istruzioni/app_istr.js
@@ -2,17 +2,6 @@ var app = angular.module('plunker', ['angularUtils.directives.dirTerminalType'])
 
 app.controller('MainCtrl', function($scope, $timeout) {
 
-   $scope.showInputForm = function() {
-    $scope.showform = true;
-    $timeout(function() {
-      document.getElementById("idInput").focus();
-    }, 50);
-  }
-
-  $scope.textEntered = function() {
-    document.getElementById("idInput").blur();
-  }
-
   goToPage = function(){
     window.open('../Prova 0/index_prova0.html', "_self");
   }
--- istruzioni/sketch_istr.js
+++ istruzioni/sketch_istr.js
@@ -11,3 +11,11 @@ function setup() {
 
 function draw() {
 }
+
+function findIndizio(){
+  document.getElementById("indizio").style.opacity = 1;
+}
+
+function mousePressed(){
+  document.getElementById("indizio").style.opacity = 0;
+}

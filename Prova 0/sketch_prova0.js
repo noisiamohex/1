1cc5730cbd6546d009389504137706312f67bea5
daniele-dellorto <37075250+daniele-dellorto@users.noreply.github.com>
2020-01-28 14:21:51 +0100

    indizio 0 e indizio istr

--- Prova 0/sketch_prova0.js
+++ Prova 0/sketch_prova0.js
@@ -45,8 +45,13 @@ function draw() {
 
 }
 
+function findIndizio(){
+  document.getElementById("indizio").style.opacity = 1;
+}
+
 function mousePressed(){
-  document.getElementById("error").style.opacity = 0;
+  document.getElementById("indizio").style.opacity = 0;
+    document.getElementById("error").style.opacity = 0;
 }
 
 goToPage = function(){
